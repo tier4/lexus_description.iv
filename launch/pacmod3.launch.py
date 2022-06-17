fb32e31bb06ed99a19a9257a9a0159a7cf12984f
Tomoya Kimura <tomoya.kimura@tier4.jp>
2022-06-17 16:41:49 +0900

    feat: replace kvaser_interface to ros2_socketcan (#34)
    * feat: replace kvaser_interface to ros2_socketcan
    
    Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>
    
    * fix namespace
    
    Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>
    
    * fix bug
    
    Signed-off-by: tomoya.kimura <tomoya.kimura@tier4.jp>

--- launch/pacmod3.launch.py
+++ launch/pacmod3.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Tier IV, Inc. All rights reserved.
+# Copyright 2022 Tier IV, Inc. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,42 +12,141 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from launch_ros.actions import LifecycleNode, Node
+from launch_ros.event_handlers import OnStateTransition
+from launch_ros.events.lifecycle import ChangeState
+from lifecycle_msgs.msg import Transition
+
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.actions import RegisterEventHandler
-from launch.event_handlers import OnProcessIO
-from launch.launch_description_sources import FrontendLaunchDescriptionSource
-from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-import yaml
+from launch.actions import DeclareLaunchArgument, EmitEvent, RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessStart
+from launch.events import matches_action
+from launch.substitutions import LaunchConfiguration, TextSubstitution
 
-
-def on_output(event):
-    card_info = yaml.safe_load(event.text.decode())
-    if "Card 0" in card_info.keys():
-        return IncludeLaunchDescription(
-            FrontendLaunchDescriptionSource(
-                [FindPackageShare("pacmod3"), "/launch/pacmod3.launch.xml"]
-            ),
-            launch_arguments={
-                "use_kvaser": "true",
-                "kvaser_hardware_id": str(card_info["Card 0"]["S/N"]),
-            }.items(),
-        )
 
 
 def generate_launch_description():
+    # pacmod3
+
+    pacmod3 = Node(
+        package="pacmod3",
+        executable="pacmod3_driver",
+        name="pacmod",
+        parameters=[{}],
+    )
+
+    # socket can receiver
+
+    socket_can_receiver_node = LifecycleNode(
+        package="ros2_socketcan",
+        executable="socket_can_receiver_node_exe",
+        name="socket_can_receiver",
+        namespace=TextSubstitution(text=""),
+        remappings=[("from_can_bus", "/pacmod/can_tx")],
+        parameters=[
+            {
+                "interface": LaunchConfiguration("interface"),
+                "interval_sec": LaunchConfiguration("interval_sec"),
+            }
+        ],
+        output="screen",
+    )
+
+    socket_can_receiver_configure_event_handler = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=socket_can_receiver_node,
+            on_start=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=matches_action(socket_can_receiver_node),
+                        transition_id=Transition.TRANSITION_CONFIGURE,
+                    ),
+                ),
+            ],
+        ),
+        condition=IfCondition(LaunchConfiguration("auto_configure")),
+    )
+
+    socket_can_receiver_activate_event_handler = RegisterEventHandler(
+        event_handler=OnStateTransition(
+            target_lifecycle_node=socket_can_receiver_node,
+            start_state="configuring",
+            goal_state="inactive",
+            entities=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=matches_action(socket_can_receiver_node),
+                        transition_id=Transition.TRANSITION_ACTIVATE,
+                    ),
+                ),
+            ],
+        ),
+        condition=IfCondition(LaunchConfiguration("auto_activate")),
+    )
+
+    # socket can sender
+
+    socket_can_sender_node = LifecycleNode(
+        package="ros2_socketcan",
+        executable="socket_can_sender_node_exe",
+        name="socket_can_sender",
+        namespace=TextSubstitution(text=""),
+        remappings=[("to_can_bus", "/pacmod/can_rx")],
+        parameters=[
+            {
+                "interface": LaunchConfiguration("interface"),
+                "timeout_sec": LaunchConfiguration("timeout_sec"),
+            }
+        ],
+        output="screen",
+    )
+
+    socket_can_sender_configure_event_handler = RegisterEventHandler(
+        event_handler=OnProcessStart(
+            target_action=socket_can_sender_node,
+            on_start=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=matches_action(socket_can_sender_node),
+                        transition_id=Transition.TRANSITION_CONFIGURE,
+                    ),
+                ),
+            ],
+        ),
+        condition=IfCondition(LaunchConfiguration("auto_configure")),
+    )
+
+    socket_can_sender_activate_event_handler = RegisterEventHandler(
+        event_handler=OnStateTransition(
+            target_lifecycle_node=socket_can_sender_node,
+            start_state="configuring",
+            goal_state="inactive",
+            entities=[
+                EmitEvent(
+                    event=ChangeState(
+                        lifecycle_node_matcher=matches_action(socket_can_sender_node),
+                        transition_id=Transition.TRANSITION_ACTIVATE,
+                    ),
+                ),
+            ],
+        ),
+        condition=IfCondition(LaunchConfiguration("auto_activate")),
+    )
+
     return LaunchDescription(
         [
-            Node(
-                package="kvaser_interface",
-                executable="list_channels",
-                name="list_channels",
-            ),
-            RegisterEventHandler(
-                OnProcessIO(
-                    on_stdout=on_output,
-                )
-            ),
+            DeclareLaunchArgument("interface", default_value="can0"),
+            DeclareLaunchArgument("timeout_sec", default_value="0.01"),
+            DeclareLaunchArgument("interval_sec", default_value="0.01"),
+            DeclareLaunchArgument("auto_configure", default_value="true"),
+            DeclareLaunchArgument("auto_activate", default_value="true"),
+            pacmod3,
+            socket_can_receiver_node,
+            socket_can_receiver_configure_event_handler,
+            socket_can_receiver_activate_event_handler,
+            socket_can_sender_node,
+            socket_can_sender_configure_event_handler,
+            socket_can_sender_activate_event_handler,
         ]
     )
